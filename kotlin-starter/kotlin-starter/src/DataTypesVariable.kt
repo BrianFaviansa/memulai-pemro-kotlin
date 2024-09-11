5b6b6a0a2340802a0d9e2f9bef3050996f048489
BrianFaviansa <brianfpd31@gmail.com>
2024-09-11 17:52:05 +0700

    [Data Types & Variable] done

---
+++ kotlin-starter/kotlin-starter/src/DataTypesVariable.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val firstWord = "Dicoding "
+    val secondWord = "Academy"
+    println(firstWord + secondWord)
+
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
+}
