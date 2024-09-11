da33f99a30f1878636496e9529c873fdd3c91879
BrianFaviansa <brianfpd31@gmail.com>
2024-09-11 18:40:30 +0700

    [If Expressions] done

---
+++ kotlin-starter/src/KotlinFundamental/IfExpressions.kt
@@ -0,0 +1,20 @@
+package KotlinFundamental
+
+fun main(args: Array<String>) {
+    val openHours = 7
+    val now = 7
+    val office: String
+    if (now > openHours) {
+        office = "Office already open"
+    } else if (now == openHours) {
+        office = "Wait a minute, office will be opened soon"
+    } else {
+        office = "Office is closed"
+    }
+
+    println(office)
+
+    /*
+    Perlu diketahui bahwa Kotlin tidak mendukung ternary operator (condition ? then : else), karena peran dari operator tersebut sudah digantikan dengan if expressions.
+     */
+}
