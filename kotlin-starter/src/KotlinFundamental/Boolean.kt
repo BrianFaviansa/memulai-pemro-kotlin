f4c1cec435bd7dd5b1f725e47ca34f13ee2662d3
BrianFaviansa <brianfpd31@gmail.com>
2024-09-11 18:53:44 +0700

    [Boolean] done

---
+++ kotlin-starter/src/KotlinFundamental/Boolean.kt
@@ -0,0 +1,30 @@
+package KotlinFundamental
+
+fun main() {
+//    Conjunction atau AND (&&)
+    val officeOpen = 7
+    val officeClosed = 17
+    val now = 15
+
+//    val isOpen = if(now >= officeOpen && now <= officeClosed) {
+//        true
+//    } else {
+//        false
+//    }
+//    println(isOpen)
+
+//    menyederhanakan if else diatas
+    val isOpen = now >= officeOpen && now <= officeClosed
+    println("Office is open : $isOpen")
+
+//    Disjunction atau OR (||)
+    val isClose = now < officeOpen || now > officeClosed
+    println("Office is closed : $isClose")
+
+//    Negation atau NOT (!)
+    if (!isOpen) {
+        println("Office is closed, come back tomorrow")
+    } else {
+        println("Office is open, happy working")
+    }
+}
