d4dab1415cc0aa5d400d8da8d00c00e0cbfde43b
BrianFaviansa <brianfpd31@gmail.com>
2024-09-11 18:29:08 +0700

    [String Template] done

---
+++ kotlin-starter/src/KotlinFundamental/StringTemplate.kt
@@ -0,0 +1,12 @@
+package KotlinFundamental
+
+fun main() {
+    val learn = "Kotlin"
+    val age = 20
+    println("I am learning $learn, I'm $age years old")
+
+    val hour = 8
+    print("Office ${if (hour > 7) "already close" else "is open"}")
+
+//    Dengan string template, kita lebih mudah membuat objek String yang dinamis.
+}
